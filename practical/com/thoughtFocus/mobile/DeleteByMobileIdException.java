75866d24c5a194b783a85e26c01ca9dc12d35608
vamsidamarla <vamsidamarla1@gmail.com>
2021-12-25 15:45:55 +0530

    deleted practical

--- practical/com/thoughtFocus/mobile/DeleteByMobileIdException.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtFocus.mobile;
-
-public class DeleteByMobileIdException extends RuntimeException {
-
-	@Override
-	public String getMessage() {
-		return "Unable to delete...... provide valid details";
-	}
-}
